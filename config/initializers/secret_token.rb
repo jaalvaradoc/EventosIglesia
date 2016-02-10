116337a24af9d9c7b98446d5ed313b4455c18bb9
Ever Melara <eamelaram@ujmd.edu.sv>
2016-02-10 09:10:50 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eventosiglesia::Application.config.secret_key_base = 'e568df175d320ecd86b8aabe38af2c6895a242a6caf0770122bd6fd4a444aa6e37f014714afc369a0b500390353fafd496b7ca942ada3d10522b8f3d66360f88'
