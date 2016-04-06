90f1ad545218559e43d27a8c5a0974610e7c5688
Javier Alvarado <jaalvaradoc@ujmd.edu.sv>
2016-04-06 07:14:57 -0600

    cambios de horario

--- app/controllers/inscripcions_controller.rb
+++ app/controllers/inscripcions_controller.rb
@@ -7,5 +7,32 @@ class InscripcionsController < ApplicationController
   def show
   	redirect_to "/users/lista-talleres"
   end
+  def new
+  	#@taller=Taller.find(params[:taller])
+  	#hobo_new
+
+  	hobo_new do
+	  	
+	  	if Taller.exists?(params[:taller])
+	  		@taller=Taller.find(params[:taller])
+	  	if !params[:project].blank?
+	    @horario = Horario.find(params[:project])
+	    else 
+	    @horario = Horario.all.first
+	    end
+		else
+			redirect_to "/"
+		end
+	end
+  end
+
+  def horarioseleccionado
+  	hor=Horario.find(params[:horario])
+
+  	render :update do |page|
+  		page.replace_html 'cupos', :partial=>'cupos', :object=>hor
+  	end
+  end
+
 
 end
