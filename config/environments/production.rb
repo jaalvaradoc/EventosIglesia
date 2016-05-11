b09c56e8356a5c3575790d12142056bcb286ccea
Javier Alvarado <javier_avarado@hotmail.com>
2016-05-11 19:59:28 +0000

    asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Eventosiglesia::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "http://dlwoxvw5bj2ff.cloudfront.net"
+  #config.action_controller.asset_host = "http://dlwoxvw5bj2ff.cloudfront.net"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
