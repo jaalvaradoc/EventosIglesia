040d9dc79b467d6adb0820e94919d75e29d8e044
Javier Alvarado <jaalvaradoc@ujmd.edu.sv>
2016-06-05 22:23:45 -0600

    casi completa

--- app/controllers/horarios_controller.rb
+++ app/controllers/horarios_controller.rb
@@ -6,7 +6,7 @@ class HorariosController < ApplicationController
 
  def crear_horario
  	@horario=Horario.new
- 	@horario.hora_inicio= (params[:horario]["hora_inicio(4i)"].to_i-6).to_s+":"+params[:horario]["hora_inicio(5i)"].to_s
+ 	@horario.hora_inicio= (params[:horario]["hora_inicio(4i)"].to_i+6).to_s+":"+params[:horario]["hora_inicio(5i)"].to_s
 	if @horario.save
     	flash[:success]="Se ha creado satisfactoriamente el horario: #{@horario.hora_inicio.to_time.strftime("%I:%M %p")}."
 	else
